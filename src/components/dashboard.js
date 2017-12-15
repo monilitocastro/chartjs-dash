88b4ab46ac8ae54c6e87ecc24f282e2a744543ff
Monilito Castro <mannycastro.epic@gmail.com>
2017-12-15 15:45:56 -0600

    TODO vertical fit

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div>Hello world!#
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+
+const Avatar = (props)=>{
+  return(
+    <div className="box avatar">
+      <p style={{textAlign:'center', fontWeight:300, fontSize: 36, margin:'auto'}}>APRICITY</p>
+    </div>
+  );
+}
+const Header = (props)=>{
+  return(
+    <div className="box appBar">
+      <AppBar
+        title="Title"
+        zDepth={0}
+      />
+    </div>
+  );
+}
+
+const Aside = (props) => {
+  return (
+    <div className="box aside">
+      <div></div>
+      <div>
+        <MenuItem>Cardiovascular Monitor</MenuItem>
+        <MenuItem>Comprehensive Metabolic Panel</MenuItem>
+        <MenuItem>Liver Function Panel</MenuItem>
       </div>
+    </div>
+  );
+}
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    
+    return (
+      <MuiThemeProvider>
+        <div className='dashboard'>
+          <Avatar />
+          <Header />
+          <Aside />
+          <div className="box d"></div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
