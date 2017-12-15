bf2be1cbbf7edde2860c3c04d1433c87d5ef677c
Monilito Castro <mannycastro.epic@gmail.com>
2017-12-15 11:58:52 -0600

    Created react app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Dashboard from './components/dashboard';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
+registerServiceWorker();
